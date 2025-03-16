771c6855b70766397a7855762a9478a95345d7f4
akemi <akemi.mullis@gmail.com>
2025-03-16 17:07:18 -0300

    Adição de funções e pequenas verificações de input

---
+++ bin/calculadora.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
+
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  void multiplicacao() {
+    print(numeroUm * numeroDois);
+  }
+
+  // pode ser nulo
+  String? entrada = stdin.readLineSync();
+
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroUm = double.parse(entrada);
+    }
+  }
+
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroDois = double.parse(entrada);
+    }
+  }
+
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    operacao = entrada;
+  }
+
+  switch (operacao) {
+    case "+":
+      soma();
+    case "-":
+      subtracao();
+    case "/":
+      divisao();
+    case "*":
+      multiplicacao();
+      break;
+  }
+}
